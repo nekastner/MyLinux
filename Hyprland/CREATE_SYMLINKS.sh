5705e281a582b444c78078d5fcaadeb8b2026cb7
Noah Elias Kastner <elias@kastner-home.com>
2026-05-14 12:02:30 +0200

    images do not get copied, but symlinked, so setup script is now create symlinks script again

--- Hyprland/CREATE_SYMLINKS.sh
+++ Hyprland/CREATE_SYMLINKS.sh
@@ -4,10 +4,8 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 ln -sf "$SCRIPT_DIR/hyprland.conf" ~/.config/hypr/hyprland.conf
 ln -sf "$SCRIPT_DIR/hyprpaper.conf" ~/.config/hypr/hyprpaper.conf
+ln -sf "$SCRIPT_DIR/hyprpaper-bg.png" ~/.config/hypr/hyprpaper-bg.png
 ln -sf "$SCRIPT_DIR/hyprlock.conf" ~/.config/hypr/hyprlock.conf
 ln -sf "$SCRIPT_DIR/hyprlock-bg.png" ~/.config/hypr/hyprlock-bg.png
 ln -sf "$SCRIPT_DIR/waybar.conf" ~/.config/waybar/config
-
-cp "$SCRIPT_DIR/hyprpaper-bg.png" ~/.config/hypr/hyprpaper-bg.png
-cp "$SCRIPT_DIR/hyprlock-bg.png" ~/.config/hypr/hyprlock-bg.png
 
