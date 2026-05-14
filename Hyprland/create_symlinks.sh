922b53d10fb2dfc0107af9c2a0f34e064bd08e22
Noah Elias Kastner <elias@kastner-home.com>
2026-05-14 10:47:52 +0200

    created script for hyprland for auto creation of symlinks

---
+++ Hyprland/create_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+ln -sf "$SCRIPT_DIR/hyprland.conf" ~/.config/hypr/hyprland.conf
+ln -sf "$SCRIPT_DIR/hyprlock.conf" ~/.config/hypr/hyprlock.conf
+ln -sf "$SCRIPT_DIR/hyprlock-bg.png" ~/.config/hypr/hyprlock-bg.png
+ln -sf "$SCRIPT_DIR/waybar.conf" ~/.config/waybar/config
+
