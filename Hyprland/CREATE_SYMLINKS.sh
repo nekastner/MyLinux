87ce164e05d9c54cc6f122ad8fd14137e9293e7f
Noah Elias Kastner <elias@kastner-home.com>
2026-05-14 11:53:37 +0200

    added config for hyprpaper

--- Hyprland/CREATE_SYMLINKS.sh
+++ Hyprland/CREATE_SYMLINKS.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 ln -sf "$SCRIPT_DIR/hyprland.conf" ~/.config/hypr/hyprland.conf
+ln -sf "$SCRIPT_DIR/hyprpaper.conf" ~/.config/hypr/hyprpaper.conf
 ln -sf "$SCRIPT_DIR/hyprlock.conf" ~/.config/hypr/hyprlock.conf
 ln -sf "$SCRIPT_DIR/hyprlock-bg.png" ~/.config/hypr/hyprlock-bg.png
 ln -sf "$SCRIPT_DIR/waybar.conf" ~/.config/waybar/config
