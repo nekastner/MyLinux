bf673408737b9e1bc6300477645dcbc5bdfd3111
Noah Elias Kastner <elias@kastner-home.com>
2026-05-16 22:29:42 +0200

    adjusted RUN_ARCHINSTALL_WITH_CONFIGS.sh (desktop_config.json -> config.json)

--- archinstall/RUN_ARCHINSTALL_WITH_CONFIGS.sh
+++ archinstall/RUN_ARCHINSTALL_WITH_CONFIGS.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-sudo archinstall --config "$SCRIPT_DIR/desktop_config.json" --creds "$SCRIPT_DIR/creds.json"
+sudo archinstall --config "$SCRIPT_DIR/config.json" --creds "$SCRIPT_DIR/creds.json"
 
