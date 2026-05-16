4ce10df752ff3747f73ffb007e811a1ce7e2c6da
Noah Elias Kastner <elias@kastner-home.com>
2026-05-16 22:50:42 +0200

    made settings vor tabulator behaviour in Nvim/init.lua

--- Nvim/init.lua
+++ Nvim/init.lua
@@ -1,19 +1,30 @@
 -- lazy vim
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- settings
+-- style and behaviour settings
 
+-- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
 
+-- tabs
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+
+-- shell commands
 vim.opt.shell = "zsh"
 vim.opt.shellcmdflag = "-ic"
+
+-- load project related configs
+vim.opt.exrc = true
 
