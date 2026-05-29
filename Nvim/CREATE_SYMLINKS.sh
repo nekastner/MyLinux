3b3ddef46f133bebb229552ee2a384e38d46842d
Noah Elias Kastner <elias@kastner-home.com>
2026-05-29 18:08:11 +0200

    removed scripts for copying and creating symlinks

--- Nvim/CREATE_SYMLINKS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-mkdir -p "$HOME/.config"
-
-rm -rf "$HOME/.config/nivm" 2> /dev/null
-ln -snf "$SCRIPT_DIR/nvim" "$HOME/.config/nvim"
-
