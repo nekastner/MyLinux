3b1992706d1ecad57da21d340ec79b56fbadc441
Noah Elias Kastner <elias@kastner-home.com>
2026-05-31 10:48:53 +0200

    moved configs from plugins-raspi.zsh into on-raspi.zsh

--- OhMyZsh/on-raspi.zsh
+++ OhMyZsh/on-raspi.zsh
@@ -2,3 +2,7 @@ alias bc="batcat"
 
 alias update="sudo ~/scripts/update.sh"
 alias backup="sudo ~/scripts/backup.sh"
+
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # always last one
+
