fa3a817d08f189e8f5d7680a527b5a8b7bde6c3c
Noah Elias Kastner <elias@kastner-home.com>
2026-05-31 10:37:44 +0200

    added alias for oppening encrypted drives

--- OhMyZsh/base.zsh
+++ OhMyZsh/base.zsh
@@ -1,13 +1,15 @@
-alias sudo="sudo "
+alias sudo='sudo '
 
 alias src="source $HOME/.zshrc"
-alias clr="clear"
-alias now="date +\"%Y/%m/%d %H:%M:%S%n week: %V | day: %u \""
+alias clr='clear'
+alias now='date +"%Y/%m/%d %H:%M:%S%n week: %V | day: %u "'
 
-alias py="python"
+alias py='python'
+alias mnt-crpt='(){ sudo cryptsetup open "$1" "$2" && sudo mount "/dev/mapper/$2" "$3"; }'
 
 bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
 bindkey '^H' backward-kill-word
 bindkey '\e[3~' delete-char
 bindkey '\e[3;5~' kill-word
+
