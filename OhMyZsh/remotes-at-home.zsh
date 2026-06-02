36a45a183c61c33cf2f59e200f4f0350a4e4570d
Noah Elias Kastner <elias@kastner-home.com>
2026-06-02 15:29:28 +0200

    fixed typy in remotes-at-home.zsh

--- OhMyZsh/remotes-at-home.zsh
+++ OhMyZsh/remotes-at-home.zsh
@@ -4,5 +4,5 @@ alias rsp-smb='smbclient //raspi/SmbShare -U user'
 
 # nas01
 alias nas01-wol='wakeonlan 90:09:D0:00:06:22'
-alias nas01-dasi='smbclient //nas01//Dasi -U Homer'
+alias nas01-dasi='smbclient //nas01/Dasi -U Homer'
 
