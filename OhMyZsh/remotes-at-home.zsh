713aacccb46e1f4ae89e52af3a04d9e61f92bbad
Noah Elias Kastner <elias@kastner-home.com>
2026-06-12 16:04:08 +0200

    renamed rsp to rpi in zsh aliases

--- OhMyZsh/remotes-at-home.zsh
+++ OhMyZsh/remotes-at-home.zsh
@@ -1,6 +1,6 @@
 # raspi
-alias rsp-ssh='ssh user@raspi'
-alias rsp-smb='smbclient //raspi/SmbShare -U user'
+alias rpi-ssh='ssh user@raspi'
+alias rpi-smb='smbclient //raspi/SmbShare -U user'
 
 # nas01
 alias nas01-wol='wakeonlan 90:09:D0:00:06:22'
