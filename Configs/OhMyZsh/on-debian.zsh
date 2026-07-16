4c3c5736a3bd8cd919c309ee759b0c05851e0894
Noah Elias Kastner <elias@kastner-home.com>
2026-07-16 08:10:09 +0000

    removed update and backup aliases for debian from zsh

--- Configs/OhMyZsh/on-debian.zsh
+++ Configs/OhMyZsh/on-debian.zsh
@@ -1,8 +1,5 @@
 alias bc="batcat"
 
-alias update="sudo ~/scripts/update.sh"
-alias backup="sudo ~/scripts/backup.sh"
-
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # always last one
 
