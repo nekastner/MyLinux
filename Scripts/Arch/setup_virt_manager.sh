570da665ae555db2f5bb7ecaf63f4d71846a247c
Noah Elias Kastner <elias@kastner-home.com>
2026-07-31 22:10:33 +0200

    removed script for setting up virt-manager on arch linux

--- Scripts/Arch/setup_virt_manager.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat iptables-nft libguestfs
-sudo systemctl enable --now libvirtd
-sudo usermod -aG libvirt $USER
