f617cee8a42b73aadadae3e238b74d4ee4432a31
Noah Elias Kastner <elias@kastner-home.com>
2026-08-24 17:59:49 +0200

    fixed alias for arch for adding pgp key (repairing installation from aur if key missing)

--- Configs/OhMyZsh/arch.zsh
+++ Configs/OhMyZsh/arch.zsh
@@ -1,2 +1,4 @@
+alias fix-pgp-key='gpg --keyserver hkps://keys.openpgp.org --recv-keys'
+
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # always last one
