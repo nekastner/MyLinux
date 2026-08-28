eb94b3372d51f4fc75ff0658a137d2360665dcab
Noah Elias Kastner <elias@kastner-home.com>
2026-08-28 23:37:00 +0200

    optimized code of deplay script for configs

--- Configs/DEPLOY.sh
+++ Configs/DEPLOY.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 CONFIGS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 sudo -v
 
 link() {
@@ -36,35 +37,35 @@ link() {
 		echo "Copied '$SOURCE' to '$TARGET'."
 	fi
 
-	if [[ "$TARGET" =~ ^/(etc|usr|boot)/ ]]; then
+	if [[ ! "$TARGET" =~ ^/home/ ]]; then
 		$EXEC chown -R root:root "$TARGET"
 	fi
 }
 
 #				SOURCE											TARGET									MODE
+link			"$CONFIGS_DIR/Refind/refind.conf"				"/boot/EFI/refind/refind.conf"			'cp'
+link			"$CONFIGS_DIR/Refind/background.png"			"/boot/EFI/refind/background.png"		'cp'
+link			"$CONFIGS_DIR/KernelPresets/linux.preset"		"/etc/mkinitcpio.d/linux.preset"		'cp'
+link			"$CONFIGS_DIR/KernelPresets/zen-linux.preset"	"/etc/mkinitcpio.d/zen-linux.preset"	'cp'
 link			"$CONFIGS_DIR/OhMyZsh/.zshrc"					"$HOME/.zshrc"							'ln'
 for config in	"$CONFIGS_DIR/OhMyZsh/"*; do
     [[ -e "$config" ]] || continue
     name=$(basename "$config")
 	link		"$config"										"$HOME/.oh-my-zsh/custom/$name"			'ln'
 done
+link			"$CONFIGS_DIR/Git/.gitconfig"					"$HOME/.gitconfig"						'ln'
 link			"$CONFIGS_DIR/Nvim"								"$HOME/.config/nvim"					'ln'
 link			"$CONFIGS_DIR/Vim/.vimrc"						"$HOME/.vimrc"							'ln'
-link			"$CONFIGS_DIR/Git/.gitconfig"					"$HOME/.gitconfig"						'ln'
 link			"$CONFIGS_DIR/Nano/.nanorc"						"$HOME/.nanorc"							'ln'
-link			"$CONFIGS_DIR/Hyprland/hypr"					"$HOME/.config/hypr"					'ln'
-link			"$CONFIGS_DIR/Hyprland/waybar"					"$HOME/.config/waybar"					'ln'
-link			"$CONFIGS_DIR/MangoHud/Mangohud"				"$HOME/.config/Mangohud"				'ln'
-link			"$CONFIGS_DIR/MimeAppList/mimeapps.list"		"$HOME/.config/mimeapps.list"			'ln'
-link			"$CONFIGS_DIR/Refind/refind.conf"				"/boot/EFI/refind/refind.conf"			'cp'
-link			"$CONFIGS_DIR/Refind/background.png"			"/boot/EFI/refind/background.png"		'cp'
-link			"$CONFIGS_DIR/KernelPresets/linux.preset"		"/etc/mkinitcpio.d/linux.preset"		'cp'
-link			"$CONFIGS_DIR/KernelPresets/zen-linux.preset"	"/etc/mkinitcpio.d/zen-linux.preset"	'cp'
-link			"$CONFIGS_DIR/Sddm/sddm.conf"					"/etc/sddm.conf"						'ln'
-link			"$CONFIGS_DIR/Sddm/themes"						"/usr/share/sddm/themes"				'ln'
 link			"$CONFIGS_DIR/Samba/smb.conf"					"/etc/samba/smb.conf"					'ln'
 link			"$CONFIGS_DIR/Samba/user_specific"				"/etc/samba/user_specific"				'ln'
 link			"$CONFIGS_DIR/Nginx/nginx.conf"					"/etc/nginx/nginx.conf"					'ln'
 link			"$CONFIGS_DIR/Nginx/sites-available"			"/etc/nginx/sites-available"			'ln'
+link			"$CONFIGS_DIR/Sddm/sddm.conf"					"/etc/sddm.conf"						'ln'
+link			"$CONFIGS_DIR/Sddm/themes"						"/usr/share/sddm/themes"				'ln'
+link			"$CONFIGS_DIR/Hyprland/hypr"					"$HOME/.config/hypr"					'ln'
+link			"$CONFIGS_DIR/Hyprland/waybar"					"$HOME/.config/waybar"					'ln'
+link			"$CONFIGS_DIR/MimeAppList/mimeapps.list"		"$HOME/.config/mimeapps.list"			'ln'
+link			"$CONFIGS_DIR/MangoHud/Mangohud"				"$HOME/.config/Mangohud"				'ln'
 
 exit 0
