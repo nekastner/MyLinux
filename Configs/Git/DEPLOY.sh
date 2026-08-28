bc76d8ad1a46aab6a79bb62e4640f6038b9ff8eb
Noah Elias Kastner <elias@kastner-home.com>
2026-08-28 16:04:36 +0200

    implemented script for linking git config

--- Configs/Git/DEPLOY.sh
+++ Configs/Git/DEPLOY.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo 'Not implemented yet!'
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+read -p "Overwrite existing '$HOME/.gitconfig'? [Y/n] " user_confirmation
+
+if [[ $user_confirmation == [nN]* ]]; then
+	echo 'Aborting...'
+	exit 0
+fi
+
+rm -f "$HOME/.gitconfig"
+ln -sf "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
+echo "Linked '$SCRIPT_DIR/.gitconfig' to '$HOME/.gitconfig'"
+
+exit 0
