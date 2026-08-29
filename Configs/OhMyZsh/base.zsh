72f63cdffeccd9ae2dd13036d97da5bb7e85b6b1
Noah Elias Kastner <elias@kastner-home.com>
2026-08-29 13:12:05 +0200

    optimized functions in base.zsh

--- Configs/OhMyZsh/base.zsh
+++ Configs/OhMyZsh/base.zsh
@@ -17,44 +17,48 @@ bindkey '^H' backward-kill-word
 bindkey '\e[3~' delete-char
 bindkey '\e[3;5~' kill-word
 
-vpy() {
+_COLOR_GREEN='\033[0;32m'
+_COLOR_RED='\033[0;31m'
+_COLOR_NEUTRAL='\033[0m'
 
+vpy()
+{
     local venv_dir="$1"
     local python_args=("${@:2}")
 
     [[ -x "$venv_dir/bin/python" ]] || {
-        print "Python venv not found: $venv_dir/bin/python" >&2
+        printf "${_COLOR_RED}Python venv not found: $venv_dir/bin/python${_COLOR_NEUTRAL}"
         return 1
     }
 
     "$venv_dir/bin/python" "${python_args[@]}"
 }
 
-vpip() {
-
+vpip()
+{
     local venv_dir="$1"
     local pip_args=("${@:2}")
 
     vpy "$venv_dir" -m pip "${pip_args[@]}"
 }
 
-mnt_crpt() {
-
+mnt_crpt()
+{
     local device=$1
     local mapper=$2
     local mountpoint=$3
 
-    (( $# == 3 )) || {
-        print "Usage: mnt-crpt <device> <mapper-name> <mountpoint>"
+    [[ $# == 3 ]] || {
+        printf "${_COLOR_RED}Usage: mnt-crpt <device> <mapper> <mountpoint>${_COLOR_NEUTRAL}"
         return 1
     }
 
     sudo cryptsetup open "$device" "$mapper" || return
 
-    sudo mount "/dev/mapper/$mapper" "$mountpoint" || {
+    sudo mount "$mapper" "$mountpoint" || {
         sudo cryptsetup close "$mapper"
         return 1
     }
 
-    print "Mounted $device -> $mountpoint"
+    printf "Mounted '$device' ('$mapper') to '$mountpoint'."
 }
