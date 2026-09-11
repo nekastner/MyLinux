02c813b85473e001a9c5d41064d35b78751122d8
Noah Elias Kastner <elias@kastner-home.com>
2026-09-11 16:07:02 +0200

    removed pip alias, system version of python has no pip

--- Configs/OhMyZsh/base.zsh
+++ Configs/OhMyZsh/base.zsh
@@ -8,7 +8,6 @@ alias clr='clear'
 alias now='date +"%Y/%m/%d %H:%M:%S%n week: %V | day: %u "'
 
 alias py='python'
-alias pip='python -m pip'
 alias py-venv="python -m venv"
 
 bindkey '^[[1;5C' forward-word
