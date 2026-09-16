bc14898fe18c97167f7b0ba4cb51a5ed2c55dd77
Noah Elias Kastner <elias@kastner-home.com>
2026-09-16 13:09:12 +0200

    optimized code of wifi.zsh

--- Configs/OhMyZsh/wifi.zsh
+++ Configs/OhMyZsh/wifi.zsh
@@ -3,14 +3,14 @@ alias wifi-ls='nmcli device wifi'
 alias wifi-st='wifi_st'
 wifi_st()
 {
-	local ifname=$1
-
 	if [[ $# != 1 ]];
 	then
 		echo "ERROR ==> Wrong usage." >&2
 		echo "Parameters: <ifname>"
 		return 1
 	fi
+
+	local ifname=$1
 	
 	nmcli device wifi show ifname "$ifname"
 	nmcli device show "$ifname"
@@ -20,15 +20,15 @@ wifi_st()
 alias wifi-con='wifi_con'
 wifi_con()
 {
-	local ifname=$1
-	lcoal ssid=$2
-
 	if [[ $# != 2 ]];
 	then
 		echo "ERROR ==> Wrong usage." >&2
 		echo "Parameters: <ifname> <ssid>"
 		return 1
 	fi
+
+	local ifname=$1
+	lcoal ssid=$2
 
 	sudo nmcli --ask device wifi connect "$ssid" ifname "ifname"
 	return 0
