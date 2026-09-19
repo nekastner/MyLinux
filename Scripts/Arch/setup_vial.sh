cf938aa43b974b434db72abca1cf6125e5fe2b95
Noah Elias Kastner <elias@kastner-home.com>
2026-09-19 11:44:17 +0200

    modified setup script for vial

--- Scripts/Arch/setup_vial.sh
+++ Scripts/Arch/setup_vial.sh
@@ -4,13 +4,10 @@ set -e
 
 # create destination directory
 cd ~
-mkdir Vial
-cd Vial
 
 # clone code
-git clone https://github.com/vial-kb/vial-gui.git
-cd vial-gui
-
+git clone https://github.com/vial-kb/vial-gui.git Vial
+cd Vial
 
 # install required python version
 sudo pacman -S --needed pyenv
@@ -25,7 +22,6 @@ venv/bin/pip install -r requirements.txt
 export USER_GID=`id -g`; sudo --preserve-env=USER_GID sh -c 'echo "KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{serial}==\"*vial:f64c2b3c*\", MODE=\"0660\", GROUP=\"$USER_GID\", TAG+=\"uaccess\", TAG+=\"udev-acl\"" > /etc/udev/rules.d/59-vial.rules && udevadm control --reload && udevadm trigger'
 
 # create executable
-cd ..
 cat << 'EOF' > RUN.sh
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")/vial-gui" && QT_QPA_PLATFORM=xcb venv/bin/fbs run
