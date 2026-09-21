b3ca3d7e2c472f36e6922e0cf8fc4c440dc84e2a
Noah Elias Kastner <elias@kastner-home.com>
2026-09-21 20:04:23 +0200

    made hdmi 2 horizontal for pc-elias

--- Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
+++ Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
@@ -5,25 +5,24 @@ hl.monitor({
 	scale = 1,
 	transform = 0,
 })
-for i = 1, 10 do
+for i = 1, 9 do
 	hl.workspace_rule({
 		workspace = i,
 		monitor = "HDMI-A-1",
 	})
 end
 
---[[
+
 hl.monitor({
 	output = "HDMI-A-2",
-	mode = "1080x1920@60",
+	mode = "1920x1080@60",
 	position = "1920x0",
 	scale = 1,
-	transform = 3,
+	transform = 0,
 })
 for i = 9, 10 do
-	hl.workspace({
-		id = i,
+	hl.workspace_rule({
+		workspace = i,
 		monitor = "HDMI-A-2",
 	})
 end
-]]
