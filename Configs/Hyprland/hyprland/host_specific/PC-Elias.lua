78e6647e034076afc6006622fbdd3ac1f3a16122
Noah Elias Kastner <elias@kastner-home.com>
2026-09-21 20:19:49 +0200

    set workspace positions on monitors for pc-elias

--- Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
+++ Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
@@ -5,7 +5,7 @@ hl.monitor({
 	scale = 1,
 	transform = 0,
 })
-for i = 1, 9 do
+for i = 1, 7 do
 	hl.workspace_rule({
 		workspace = i,
 		monitor = "HDMI-A-1",
@@ -20,7 +20,7 @@ hl.monitor({
 	scale = 1,
 	transform = 0,
 })
-for i = 9, 10 do
+for i = 8, 10 do
 	hl.workspace_rule({
 		workspace = i,
 		monitor = "HDMI-A-2",
