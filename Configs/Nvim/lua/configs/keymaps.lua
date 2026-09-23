12c6e1905ebe231cd6923d36cb2ff4471699b311
Noah Elias Kastner <elias@kastner-home.com>
2026-09-23 12:25:34 +0200

    added backtab to nvim

--- Configs/Nvim/lua/configs/keymaps.lua
+++ Configs/Nvim/lua/configs/keymaps.lua
@@ -1,2 +1,12 @@
-vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-Del>", "<C-o>de", { noremap = true, silent = true })
+local opts = { silent = true }
+
+-- delete word left
+vim.keymap.set("i", "<C-BS>",		"<C-w>",	opts)
+vim.keymap.set("i", "<C-H>",		"<C-w>",	opts)
+
+-- delete word right
+vim.keymap.set("i", "<C-Del>",		"<C-o>de",	opts)
+
+-- backtab
+vim.keymap.set("i", "<S-Tab>",		"<C-d>",	opts)
+vim.keymap.set("i", "<BackTab>",	"<C-d>",	opts)
