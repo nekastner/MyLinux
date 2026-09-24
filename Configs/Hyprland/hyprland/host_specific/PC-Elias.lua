8c8e3aa87129bb1f826b27ea904a03ce39d7b0ba
Noah Elias Kastner <elias@kastner-home.com>
2026-09-24 17:56:38 +0200

    moved workspaces 1 to 10 to monitor and workspaces 11 to 20 to monitor 2 for pc-elias in hyprlandb

--- Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
+++ Configs/Hyprland/hyprland/host_specific/PC-Elias.lua
@@ -5,7 +5,7 @@ hl.monitor({
 	scale = 1,
 	transform = 0,
 })
-for i = 1, 7 do
+for i = 1, 10 do
 	hl.workspace_rule({
 		workspace = i,
 		monitor = "HDMI-A-1",
@@ -20,7 +20,7 @@ hl.monitor({
 	scale = 1,
 	transform = 0,
 })
-for i = 8, 10 do
+for i = 11, 20 do
 	hl.workspace_rule({
 		workspace = i,
 		monitor = "HDMI-A-2",
