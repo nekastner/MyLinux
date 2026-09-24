71438e5651d01293571fad22afafd4ec48cd2b93
Noah Elias Kastner <elias@kastner-home.com>
2026-09-24 22:33:40 +0200

    removed redundant backtab key bind, which caused cursor delay for the '<' character

--- Configs/Nvim/lua/configs/keymaps.lua
+++ Configs/Nvim/lua/configs/keymaps.lua
@@ -1,12 +1,11 @@
 local opts = { silent = true }
 
 -- delete word left
-vim.keymap.set("i", "<C-BS>",		"<C-w>",	opts)
-vim.keymap.set("i", "<C-H>",		"<C-w>",	opts)
+vim.keymap.set("i", "<C-BS>", "<C-w>", opts)
+vim.keymap.set("i", "<C-H>", "<C-w>", opts)
 
 -- delete word right
-vim.keymap.set("i", "<C-Del>",		"<C-o>de",	opts)
+vim.keymap.set("i", "<C-Del>", "<C-o>de", opts)
 
 -- backtab
-vim.keymap.set("i", "<S-Tab>",		"<C-d>",	opts)
-vim.keymap.set("i", "<BackTab>",	"<C-d>",	opts)
+vim.keymap.set("i", "<S-Tab>", "<C-d>", opts)
