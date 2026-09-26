5aec092c11f4d116f16773deaa61c8d6b70b3ed0
Noah Elias Kastner <elias@kastner-home.com>
2026-09-26 15:28:30 +0200

    added a few layout settings

--- Configs/Hyprland/hyprland/design.lua
+++ Configs/Hyprland/hyprland/design.lua
@@ -1,8 +1,8 @@
-hl.curve("easeOutQuint",	{ type = "bezier", points = { { 0.23, 1 },		{ 0.32, 1 } } })
-hl.curve("easeInOutCubic",	{ type = "bezier", points = { { 0.65, 0.05 },	{ 0.36, 1 } } })
-hl.curve("linear",			{ type = "bezier", points = { { 0, 0 },			{ 1, 1 } } })
-hl.curve("almostLinear",	{ type = "bezier", points = { { 0.5, 0.5 },		{ 0.75, 1 } } })
-hl.curve("quick",			{ type = "bezier", points = { { 0.15, 0 },		{ 0.1, 1 } } })
+hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
+hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
+hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
 
 hl.animation({
 	leaf = "global",
@@ -188,6 +188,12 @@ hl.config({
 	},
 	animations = {
 		enabled = true,
+	},
+	master = {
+		new_status = "master",
+	},
+	scrolling = {
+		direction = "right",
 	},
 	misc = {
 		force_default_wallpaper = 1,
