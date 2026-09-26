d7e53ddd75c7790822d8c94cdbd1e11d230321d1
Noah Elias Kastner <elias@kastner-home.com>
2026-09-26 11:30:35 +0200

    set leader key for nvim

--- Configs/Nvim/init.lua
+++ Configs/Nvim/init.lua
@@ -1,16 +1,17 @@
+-- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- lazy vim
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
 		lazypath })
 end
-
 vim.opt.rtp:prepend(lazypath)
-
 require("lazy").setup("plugins")
 
+-- configs
 require("configs.options")
 require("configs.keymaps")
 
