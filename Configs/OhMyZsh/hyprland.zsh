cb36d6b1ed2ea6dbda7a9d3b95ef122e094e2882
Noah Elias Kastner <elias@kastner-home.com>
2026-09-26 12:03:13 +0200

    added aliases for stopping and starting hyprpaper to zsh

--- Configs/OhMyZsh/hyprland.zsh
+++ Configs/OhMyZsh/hyprland.zsh
@@ -3,6 +3,9 @@ alias quit-hyprland='command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || 
 alias stop-hyprsunset='pkill hyprsunset'
 alias start-hyprsenset='hyprctl dispatch "hl.dsp.exec_cmd(\"hyprsunset\")"'
 
+alias stop-hyprpaper='pkill hyprpaper'
+alias start-hyprpaper='hyprctl dispatch "hl.dsp.exec_cmd(\"hyprpaper\")"'
+
 alias to-clipboard='to_clipboard'
 to_clipboard()
 {
