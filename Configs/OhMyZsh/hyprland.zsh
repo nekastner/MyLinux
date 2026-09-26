08b95bea1e634ac3b2ede3c30b22e557525c344f
Noah Elias Kastner <elias@kastner-home.com>
2026-09-26 13:42:36 +0200

    added waybar stop and start alias to zsh as well as a execute command in hyprland function

--- Configs/OhMyZsh/hyprland.zsh
+++ Configs/OhMyZsh/hyprland.zsh
@@ -1,10 +1,20 @@
 alias quit-hyprland='command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit'
 
+alias stop-waybar='pkill waybar'
+alias start-waybar='hyprland_exec waybar'
+
 alias stop-hyprsunset='pkill hyprsunset'
-alias start-hyprsenset='hyprctl dispatch "hl.dsp.exec_cmd(\"hyprsunset\")"'
+alias start-hyprsenset='hyprland_exec hyprsunset'
 
 alias stop-hyprpaper='pkill hyprpaper'
-alias start-hyprpaper='hyprctl dispatch "hl.dsp.exec_cmd(\"hyprpaper\")"'
+alias start-hyprpaper='hyprland_exec hyprpaper'
+
+alias hyprland-exec='hyprland_exec'
+hyprland_exec()
+{
+	local command="$@"
+	hyprctl dispatch "hl.dsp.exec_cmd(\"$command\")"
+}
 
 alias to-clipboard='to_clipboard'
 to_clipboard()
